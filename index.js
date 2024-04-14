c704d0df92743488017b1fb2d44d1392dc44fb0a
muhammadfahadfarooq <fahad.farooq7013@gmail.com>
2024-04-13 22:06:07 -0700

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6"
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("You guessed correct number.");
+}
+else {
+    console.log(`Sorry! You guessed wrong number , correct number was : ${randomNumber}`);
+}
